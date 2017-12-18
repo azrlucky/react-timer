d6cdf43d332c7dcf4cb4df9901d96b50f68a81b2
AZR <azr@AZR-MAC.local>
2017-12-19 04:14:44 +0530

    Timer section completed with tests

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,56 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 
-var Timer = () => {
-  return (
-    <p>Timer component</p>
-  );
-};
+var Clock = require('Clock');
+var Controls = require('Controls')
+
+var Timer = React.createClass({
+  getInitialState: function() {
+    return ({
+      count: 0,
+      timerStatus: 'stopped'
+    });
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if (this.state.timerStatus !== prevState.timerStatus) {
+      switch(this.state.timerStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+  startTimer: function() {
+    this.timer  = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount >=0 ? newCount : 0
+      });
+    }, 1000);
+  },
+  handleStatusChange: function(newStatus) {
+    this.setState({timerStatus: newStatus});
+  },
+  render: function() {
+    var {count,timerStatus} = this.state;
+    return (
+      <div className="timer">
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+      </div>
+    )
+  }
+});
 
 module.exports = Timer;
